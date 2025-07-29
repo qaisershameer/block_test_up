6673f38ef24e8672fad34188dfcf00c2a4107089
Qaiser Shameer <qaiser.shameer007@gmail.com>
2025-07-29 18:06:17 +0500

    first commit counter app

---
+++ android/app/src/main/kotlin/com/block/test/block_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.block.test.block_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:block_test/bloc/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:block_test/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}

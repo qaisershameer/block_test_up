28094900035b378b3c0fe5fd9a82035a698f5eb6
Qaiser Shameer <qaiser.shameer007@gmail.com>
2025-07-31 18:26:34 +0500

    Jul 31 KBS

---
+++ lib/bloc/login/login_bloc.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:block_test/bloc/login/login_event.dart';
+import 'package:block_test/bloc/login/login_state.dart';
+import 'package:block_test/data/repositories/auth_repo.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState>{
+
+  final _authRepo = AuthRepo();
+
+  LoginBloc() : super(LoginState(isLoading: false, isSuccess: false)){
+
+    on<LoginUserEvent>(_loginUser);
+
+  }
+
+  /// [Login] - Function to login the user
+  Future<void> _loginUser(LoginUserEvent event, Emitter<LoginState> emit) async {
+    try {
+
+      /// Start Loading
+      emit(state.copyWith(isLoading: true, isSuccess: false));
+
+      // login user
+      await _authRepo.loginUser(event.userName, event.password);
+
+      print('Success');
+
+      // stop loading
+      emit(state.copyWith(isLoading: false, isSuccess: true));
+
+    } catch (e) {
+      debugPrint('Error while login: $e');
+    }
+  }
+
+}
